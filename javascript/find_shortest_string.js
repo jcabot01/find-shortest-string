d8fd445c6fd968bda622b7e93fd00b71a2c71906
JonCabot <jfc0053@gmail.com>
2022-01-17 20:12:27 -0600

    completed assignment

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,5 @@
 function findShortestString(arr) {
-  // type your code here
+  return arr.reduce((a,b) => a.length <= b.length ? a : b);
 }
 
 if (require.main === module) {
@@ -23,4 +23,13 @@ if (require.main === module) {
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+//take in an array of strings
+//find the shortest string with .reduce(previous value, current value), loop thru and compare
+//if the length of the previous string is lessorequalto the current string, return previous string, otherwise return the current string 
+//since the reduce() will return the shortest string, I need to return the function at the onset
+
 // And a written explanation of your solution
+//perform a reduce() method on the given array.  
+//Compare the previous value with the current value. 
+//If that remains true, return the first string.  
+//Otherwise, return the current string because the length of it must be less by comparison.
